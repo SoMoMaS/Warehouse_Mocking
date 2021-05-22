4e60f9a0835e35dee164bd8f003824d21e286eb0
SoMoMaS <soma.matyas.molnar@gmail.com>
2021-05-22 22:19:30 +0200

    Added some tests for the order class

--- WarehouseMocking/WareHouse/Order.cs
+++ WarehouseMocking/WareHouse/Order.cs
@@ -1,16 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//----------------------------------------------------------------------
+// <copyright file=".cs" company="FHWN.ac.at">
+// Copyright (c) FHWN. All rights reserved.
+// </copyright>
+// <summary></summary>
+// <author>Soma Molnar</author>
+// -----------------------------------------------------------------------
+
 using System.Runtime.CompilerServices;
-using System.Text;
 
 [assembly: InternalsVisibleTo("WarehouseTests")]
 namespace WareHouse
 {
-    
-    class Order
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Order" />.
+    /// </summary>
+    internal class Order
     {
+        /// <summary>
+        /// Defines the product.
+        /// </summary>
         private string product;
+
+        /// <summary>
+        /// Defines the amount.
+        /// </summary>
         private int amount;
+
+        /// <summary>
+        /// Defines the isFilledCalled.
+        /// </summary>
+        private bool isFilledCalled;
+
+        /// <summary>
+        /// Defines the isCanFillOrderCalled.
+        /// </summary>
+        private bool isCanFillOrderCalled;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Order"/> class.
+        /// </summary>
+        /// <param name="product">The product<see cref="string"/>.</param>
+        /// <param name="amount">The amount<see cref="int"/>.</param>
         public Order(string product, int amount)
         {
             if (string.IsNullOrEmpty(product))
@@ -21,30 +53,50 @@ namespace WareHouse
 
             this.product = product;
             this.amount = amount;
+            this.isFilledCalled = false;
+            this.isCanFillOrderCalled = false;
         }
 
+        /// <summary>
+        /// The isFilled.
+        /// </summary>
+        /// <returns>The <see cref="bool"/>.</returns>
         internal bool isFilled()
         {
-            throw new NotImplementedException();
+            return this.isFilledCalled;
         }
 
+        /// <summary>
+        /// The CanFillOrder.
+        /// </summary>
+        /// <param name="warehouse">The warehouse<see cref="IWarehouse"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
         internal bool CanFillOrder(IWarehouse warehouse)
         {
             bool hasProduct = warehouse.HasProduct(product);
             int stock = warehouse.CurrentStock(product);
-
+            this.isCanFillOrderCalled = true;
             return hasProduct && stock > this.amount;
         }
 
+        /// <summary>
+        /// The Fill.
+        /// </summary>
+        /// <param name="warehouse">The warehouse<see cref="IWarehouse"/>.</param>
         internal void Fill(IWarehouse warehouse)
         {
-            try
+            if (this.isCanFillOrderCalled)
             {
-                warehouse.TakeStock(this.product, this.amount);
-            }
-            catch(Exception)
-            {
-                throw;
+                try
+                {
+                    warehouse.TakeStock(this.product, this.amount);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                this.isFilledCalled = true;
             }
         }
     }
---
+++ WarehouseMocking/WarehouseTests/OrderTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WareHouse;
+
+namespace WarehouseTests
+{
+    [TestClass]
+    class OrderTests
+    {
+        [TestMethod]
+        [DataRow("toothpaste", 2)]
+        [DataRow("running shoes", 99)]
+        [DataRow("shirt", 7)]
+        public void Simple_Order_Created_Cannot_Be_Fulfilled_Should_Return_False(string product, int amount)
+        {
+            var warehouse = new SimpleWarehouse();
+            warehouse.AddStock(product, 1);
+
+            var order = new Order(product, amount);
+            bool canBeFulfilled = order.CanFillOrder(warehouse);
+            Assert.IsFalse(canBeFulfilled);
+        }
+
+        [TestMethod]
+        [DataRow("toothpaste", 2)]
+        [DataRow("running shoes", 99)]
+        [DataRow("shirt", 7)]
+        public void Simple_Order_Created_Can_Be_Fulfilled_Should_Return_True(string product, int amount)
+        {
+            var warehouse = new SimpleWarehouse();
+            warehouse.AddStock(product, 100);
+
+            var order = new Order(product, amount);
+            bool canBeFulfilled = order.CanFillOrder(warehouse);
+            Assert.IsFalse(canBeFulfilled);
+        }
+    }
+}
