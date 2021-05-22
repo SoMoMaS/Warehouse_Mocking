fb8f3c14a306db8fc1f76ab79bbf0b01f580d5cc
SoMoMaS <soma.matyas.molnar@gmail.com>
2021-05-22 19:31:58 +0200

    Added specific exceptions for this project

---
+++ WarehouseMocking/WareHouse/Exceptions/InsufficientStockException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WareHouse.Exceptions
+{
+    class InsufficientStockException : Exception
+    {
+    }
+}
---
+++ WarehouseMocking/WareHouse/Exceptions/NoSuchProductException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WareHouse.Exceptions
+{
+    class NoSuchProductException : Exception
+    {
+    }
+}
