77b12d99fde14c200f2611b2c8bbf742a53b84e9
SoMoMaS <soma.matyas.molnar@gmail.com>
2021-05-22 19:27:24 +0200

    Added order class

---
+++ WarehouseMocking/WareHouse/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WareHouse
+{
+    class Order
+    {
+        private string product;
+        private int amount;
+        public Order(string product, int amount)
+        {
+            if (string.IsNullOrEmpty(product))
+                throw new InvalidOperationException("Product name can't be empty");
+
+            if (amount < 1)
+                throw new InvalidOperationException("Amount can't be smaller than 1.");
+
+            this.product = product;
+            this.amount = amount;
+        }
+
+        internal bool isFilled()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool CanFillOrder(IWarehouse warehouse)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Fill(IWarehouse warehouse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
