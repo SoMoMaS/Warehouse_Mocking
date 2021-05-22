32bddb4dd000d0292357684955f9593dec6ce989
SoMoMaS <soma.matyas.molnar@gmail.com>
2021-05-22 18:36:02 +0200

    Interface and simple warehouse added

---
+++ WarehouseMocking/WareHouse/IWarehouse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WareHouse
+{
+    public interface IWarehouse
+    {
+        bool HasProduct(string product);
+        int CurrentStock(string product);
+
+        void AddStock(string product, int amount);
+
+        void TakeStock(string product, int amount);
+    }
+}
---
+++ WarehouseMocking/WareHouse/SimpleWarehouse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WareHouse
+{
+    class SimpleWarehouse : IWarehouse
+    {
+        public void AddStock(string product, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CurrentStock(string product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasProduct(string product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeStock(string product, int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
